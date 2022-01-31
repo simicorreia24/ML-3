dca612c2627dcfb10d3d925244a89e9134754f01
Simi Silvester Correia <66421852+simicorreia24@users.noreply.github.com>
2022-02-01 00:29:48 +0530

    Add files via upload

---
+++ ML- 3-2.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 1.Explain the term machine learning, and how does it work? Explain two machine learning applications in the business world. What are some of the ethical concerns that machine learning applications could raise?
+
+# In[ ]:
+
+
+# Machine learning is a data analytics technique that teaches computers to do what comes naturally to humans and animals
+# Real-time chatbot systems, Fraud detection etc
+# Lack quality data, cost to innovation etc
+
+
+# ## 2. Describe the process of human learning:
+# ## i. Under the supervision of experts
+# 
+# ## ii. With the assistance of experts in an indirect manner
+# 
+# ## iii. Self-education
+
+# In[ ]:
+
+
+# 1) Error is minimum just like supervised Ml
+# 2) just like unsupervised ml we know the input but are notvsure about the output
+# 3) In self education we learn from our positives and try to avoid our negatives just like reinforcement learning in ml
+
+
+# ## 3. Provide a few examples of various types of machine learning.
+
+# In[ ]:
+
+
+# 1 Supervised- regression, Classification
+# 2) Unsupervised - k means, pca
+# 3) Reinforcement learning- Positives and negatives
+
+
+# ## 4. Examine the various forms of machine learning.
+
+# In[ ]:
+
+
+# 1 Supervised- regression, Classification
+# 2) Unsupervised - k means, pca
+# 3) Reinforcement learning- Positives and negatives
+
+
+# ## 5. Can you explain what a well-posed learning problem is? Explain the main characteristics that must be present to identify a learning problem properly.
+
+# In[ ]:
+
+
+# If the solution to it exists, if the model depends on the data but at the same time is not sensative to the change in the data
+
+
+# ## 6. Is machine learning capable of solving all problems? Give a detailed explanation of your answer.
+
+# In[ ]:
+
+
+# ML is capable of solving various problems but at the same time somtimes there are problems like overfitting and underfitting
+
+
+# ## 7. What are the various methods and technologies for solving machine learning problems? Any two of them should be defined in detail.
+
+# In[ ]:
+
+
+# scikit learn-Simple and efficient tools for data mining and data analysis. It features various classification, 
+# regression and clustering algorithms including support vector machines, random forests, gradient boosting, k-means, etc. Accessible to everybody and reusable in various contexts 
+
+# tensorflow-TensorFlow is an end-to-end open source platform for machine learning. It has a comprehensive, flexible 
+# ecosystem of tools, libraries and community resources that lets researchers push the state-of-the-art in ML and developers easily build and deploy ML powered applications.
+
+
+# ## 8. Can you explain the various forms of supervised learning? Explain each one with an example application.
+
+# In[ ]:
+
+
+# supervised learning
+# Regression- continous data
+# Classification- discrete data
+# Common classification algorithms are linear classifiers, support vector machines (SVM), decision trees, k-nearest
+# neighbor, and random forest, which are described in more detail below.
+
+
+# ## 9. What is the difference between supervised and unsupervised learning? With a sample application in each region, explain the differences.
+
+# In[ ]:
+
+
+#The main difference between supervised and unsupervised learning: Labeled data. The main distinction between the 
+#two approaches is the use of labeled datasets. To put it simply, supervised learning uses labeled input and output 
+#data, while an unsupervised learning algorithm does not.
+
+
+# ## 10. Describe the machine learning process in depth.
+# 
+# ## a. Make brief notes on any two of the following:
+# 
+# ## MATLAB is one of the most widely used programming languages.
+# 
+# ## ii. Deep learning applications in healthcare
+# 
+# ## iii. Study of the market basket
+# 
+# ## iv. Linear regression (simple)
+
+# In[ ]:
+
+
+# Deep learning models can interpret medical images like X-ray, MRI scan, CT scan, etc., to perform diagnosis.
+# The algorithms can detect any risk and flag anomalies in the medical images. Deep learning is extensively used
+# in detecting cancer
+
+# Regression analysis makes use of mathematical models to describe relationships. For example, suppose that 
+# height was the only determinant of body weight. 
+
+
+# ## 11. Make a comparison between:-
+# 
+# ## 1. Generalization and abstraction
+# 
+# ## 2. Learning that is guided and unsupervised
+# 
+# ## 3. Regression and classification
+
+# In[ ]:
+
+
+# Abstraction is the process of removing details of objects. ... A generalization, then, is the formulation of general concepts from specific instances by abstracting common properties.
+# A concrete object can be looked at as a “subset” of a more generalized object.
+
+# Supervised learning algorithms are trained using labeled data. Unsupervised learning algorithms are trained using unlabeled data. Supervised learning model takes direct feedback to 
+# check if it is predicting correct output or not. Unsupervised learning model does not take any feedback
+
+# Classification is the task of predicting a discrete class label.
+# Regression is the task of predicting a continuous quantity
+
